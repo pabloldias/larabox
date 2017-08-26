d5e24c011bd6dbe4483484427b9b2b4f2b81771b
Pablo Lima Dias <pabloidz@gmail.com>
2017-08-25 23:11:18 -0300

    Mapping posts and comments

--- app/Comment.php
+++ app/Comment.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Comment extends Model
 {
-    //
+    public function post() 
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
