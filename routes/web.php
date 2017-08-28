433798ea3a9b8b88203dd5595c2b5f2e72b52e97
Pablo Lima Dias <pabloidz@gmail.com>
2017-08-27 22:56:24 -0300

    New controllers

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::post('/posts', 'PostController@store');
 Route::get('/posts/{post}', 'PostController@show');
 
 Route::post('/posts/{post}/comments', 'CommentController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::get('/login', 'SessionController@create');
