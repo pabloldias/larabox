828727c73ac290d4539a30e1e76927073da18a30
Pablo Lima Dias <pabloidz@gmail.com>
2017-08-30 20:56:25 -0300

    Filter archives by year and month

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,8 +14,17 @@ class PostController extends Controller
 
     public function index() 
     {
-        $posts = Post::latest()->get();
-        return view('posts.index', compact('posts'));
+        $posts = Post::latest()
+            ->filter(request(['year', 'month']))
+            ->get();
+
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')
+            ->orderByRaw('min(created_at) desc')
+            ->get()
+            ->toArray();
+
+        return view('posts.index', compact('posts', 'archives'));
     }
 
     public function create() 
