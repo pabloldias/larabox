657568e2e9af88b4ab8242fb6de05ebd242367df
Pablo Lima Dias <pabloidz@gmail.com>
2017-08-30 23:06:14 -0300

    Using view composer for archives

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,13 +18,7 @@ class PostController extends Controller
             ->filter(request(['year', 'month']))
             ->get();
 
-        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at) desc')
-            ->get()
-            ->toArray();
-
-        return view('posts.index', compact('posts', 'archives'));
+        return view('posts.index', compact('posts'));
     }
 
     public function create() 
